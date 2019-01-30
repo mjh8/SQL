a04cb6d510a738548b82ce030634aaa62c896f50
mjh8 <46362776+mjh8@users.noreply.github.com>
2019-01-30 07:41:03 -0600

    Add files via upload

---
+++ Data_Preprocessing_R.R
@@ -0,0 +1,67 @@
+# Data Preprocessing R
+
+
+
+
+# FINAL TEMPLATE
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+
+
+
+# FULL TEMPLATE WITH NOTES
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking Care of Missing Data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x,na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding Categorical Data
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+
+# install.packages('caTools')
+
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
