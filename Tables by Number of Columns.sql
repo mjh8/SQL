362536d7d6bdb89db167be159f7cc70ae584e1d7
mjh8 <46362776+mjh8@users.noreply.github.com>
2019-02-13 21:49:34 -0600

    Add files via upload

---
+++ Tables by Number of Columns.sql
@@ -0,0 +1,9 @@
+select schema_name(tab.schema_id) as schema_name, 
+       tab.name as table_name, 
+       count(*) as columns
+  from sys.tables as tab
+       inner join sys.columns as col
+           on tab.object_id = col.object_id 
+ group by schema_name(tab.schema_id), 
+       tab.name
+ order by count(*) desc;
